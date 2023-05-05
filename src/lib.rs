2c9e1d2d5bb59254d294102857c069057eae2597
Brice SAUNIER DEBES <brice.saunier@gmail.com>
2023-05-05 15:58:48 +0200

    refactor: moved routes in their own modules

--- src/lib.rs
+++ src/lib.rs
@@ -3,33 +3,3 @@
 pub mod configuration;
 pub mod routes; 
 pub mod startup;
-
-use std::net::TcpListener;
-
-use actix_web::{dev::Server, web, App, HttpResponse, HttpServer, Responder};
-
-#[derive(Debug, serde::Deserialize)]
-struct FormData {
-    email: String,
-    name: String,
-}
-
-pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| {
-        App::new()
-            .route("/health_check", web::get().to(health_check))
-            .route("/subscriptions", web::post().to(subscribe))
-    })
-    .listen(listener)?
-    .run();
-
-    Ok(server)
-}
-
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok().finish()
-}
-
-async fn subscribe(_form: web::Form<FormData>) -> impl Responder {
-    HttpResponse::Ok().finish()
-}

99466ab028741c7023715ec4b7121ff912eb87bc
Brice SAUNIER DEBES <brice.saunier@gmail.com>
2023-05-08 17:44:49 +0200

    feat(telemetry): parametrize log output channel

--- src/telemetry.rs
+++ src/telemetry.rs
@@ -2,18 +2,7 @@ use tracing::subscriber::set_global_default;
 use tracing::Subscriber;
 use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
 use tracing_log::LogTracer;
-use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
-
-/// Register a subscriber as global default to process span data.
-///
-/// It should only ba called once!
-pub fn init_log_subscriber(subscriber: impl Subscriber + Send + Sync) {
-    LogTracer::init().expect("Failed to set logger");
-
-    // 'set_global_default' can be used by applicarions to specify
-    // what subscriber should be used to process spans
-    set_global_default(subscriber).expect("Failed to set subscriber");
-}
+use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry, fmt::MakeWriter};
 
 /// Compose multiple layers into a `tracing`'s subscriber.
 ///
@@ -25,10 +14,22 @@ pub fn init_log_subscriber(subscriber: impl Subscriber + Send + Sync) {
 /// We need to explicitly call out that the returned subscriber is
 /// `Send` and `Sync` to make it possible to pass it to `init_subscriber`
 /// later on.
-pub fn get_log_subscriber(name: String, env_filter: String) -> impl Subscriber + Send + Sync {
+pub fn get_log_subscriber<Sink>(
+    name: String,
+    env_filter: String,
+    sink: Sink,
+) -> impl Subscriber + Sync + Send
+where
+    // This "weird" syntax is a higher-ranked trait bound (HRTB)
+    // It basically means that Sink implements the `MakeWriter`
+    // trait for all choices of the lifetime parameter `'a`
+    // Check out https://doc.rust-lang.org/nomicon/hrtb.html
+    // for more details.
+    Sink: for<'a> MakeWriter<'a> + Send + Sync + 'static,
+{
     let env_filter =
         EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new(env_filter));
-    let formatting_layer = BunyanFormattingLayer::new(name, std::io::stdout);
+    let formatting_layer = BunyanFormattingLayer::new(name, sink);
 
     Registry::default()
         .with(env_filter)
@@ -36,3 +37,13 @@ pub fn get_log_subscriber(name: String, env_filter: String) -> impl Subscriber +
         .with(formatting_layer)
 }
 
+/// Register a subscriber as global default to process span data.
+///
+/// It should only be called once!
+pub fn init_log_subscriber(subscriber: impl Subscriber + Send + Sync) {
+    LogTracer::init().expect("Failed to set logger");
+
+    // 'set_global_default' can be used by applicarions to specify
+    // what subscriber should be used to process spans
+    set_global_default(subscriber).expect("Failed to set subscriber");
+}
